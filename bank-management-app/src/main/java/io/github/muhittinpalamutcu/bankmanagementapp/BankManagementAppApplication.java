7ea6ff9789d2975928a469ec5299cb213825517d
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2021-09-23 21:17:11 +0200

    Added swagger configuration

--- bank-management-app/src/main/java/io/github/muhittinpalamutcu/bankmanagementapp/BankManagementAppApplication.java
+++ bank-management-app/src/main/java/io/github/muhittinpalamutcu/bankmanagementapp/BankManagementAppApplication.java
@@ -2,8 +2,10 @@ package io.github.muhittinpalamutcu.bankmanagementapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BankManagementAppApplication {
 
     public static void main(String[] args) {
